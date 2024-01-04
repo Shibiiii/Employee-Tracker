2f70823bd45d165182e2016fb7195022ad889590
John <purplejohn13@gmail.com>
2024-01-04 14:46:20 -0800

    insert into employee written

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,11 @@ INSERT INTO role
     ('Salesperson', 75000, 2),
     ('Accountant', 125000, 3),
     ('Lawyer', 200000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+    VALUES
+    ('John', 'Doe', 1, 4),
+    ('Jane', 'Does', 2, 3),
+    ('Juan', 'Dos', 3, 2),
+    ('Michael', 'Alvarez', 4, 1);
