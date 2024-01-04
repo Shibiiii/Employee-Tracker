ce09513336b450d336f31bec76a21e89a5c563dd
John <purplejohn13@gmail.com>
2024-01-04 14:51:55 -0800

    host,user,password, and database written

--- db/connections.js
+++ db/connections.js
@@ -1,7 +1,10 @@
 const mysql = require('mysql12');
 
 const db = mysql.createConnection({
-    
+    host: 'localhost',
+    user: 'root',
+    password: '120803',
+    databnase: 'employee-tracker-db'
 });
 
 module.exports = db;
