8f738691eb3e0463f92d91420f45e2508af9d613
John <purplejohn13@gmail.com>
2024-01-04 13:44:08 -0800

    user prompts set up

--- index.js
+++ index.js
@@ -8,5 +8,12 @@ db.connect(err => {
 });
 
 let employeeTracker = function() {
-    
+    inquirer.prompt([{
+        type: 'list',
+        name: 'prompt',
+        message: 'What would you like to do',
+        choices: ['View All Department', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Log Out']
+    }]).then((answers) => {
+        
+    })
 }
