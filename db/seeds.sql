260e793adbd01670af850c56a099f5ea29c27dcd
John <purplejohn13@gmail.com>
2024-01-04 14:41:07 -0800

    insert into roles filled oout

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,10 @@ INSERT INTO department
     ('Finance'),
     ('Legal');
 
+INSERT INTO role
+    (title, salary, department_id)
+    VALUES
+    ('Software engineer', 85000, 1),
+    ('Salesperson', 75000, 2),
+    ('Accountant', 125000, 3),
+    ('Lawyer', 200000, 4);
