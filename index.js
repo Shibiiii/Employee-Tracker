0d186ac080b84648f3c1874c96554392db1d6756
John <purplejohn13@gmail.com>
2024-01-04 20:53:22 -0800

    fixed missing colon

--- index.js
+++ index.js
@@ -7,35 +7,36 @@ db.connect(err => {
     employeeTracker();
 });
 
-var employeeTracker = function () {
+let employeeTracker = function () {
     inquirer.prompt([{
+        // Begin Command Line
         type: 'list',
         name: 'prompt',
         message: 'What would you like to do?',
         choices: ['View All Department', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Log Out']
     }]).then((answers) => {
+        // Views the Department Table in the Database
         if (answers.prompt === 'View All Department') {
             db.query(`SELECT * FROM department`, (err, result) => {
                 if (err) throw err;
                 console.log("Viewing All Departments: ");
                 console.table(result);
-            employeeTracker();
+                employeeTracker();
             });
         } else if (answers.prompt === 'View All Roles') {
             db.query(`SELECT * FROM role`, (err, result) => {
                 if (err) throw err;
                 console.log("Viewing All Roles: ");
                 console.table(result);
-            employeeTracker();
+                employeeTracker();
             });
-        } else if (amswers.prompt === 'View All Employees') {
+        } else if (answers.prompt === 'View All Employees') {
             db.query(`SELECT * FROM employee`, (err, result) => {
                 if (err) throw err;
-                console.log('Viewing All Employees');
+                console.log("Viewing All Employees: ");
                 console.table(result);
-                employeeTracker()
+                employeeTracker();
             });
-            
         }
-    })
+});
 };
